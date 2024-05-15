61f027c9204a93ae3841b9101bd2e93a6ccc93dd
ValhallaRising74 <valhalla.rising.viking@gmail.com>
2024-05-15 15:47:06 -0300

    Add files via upload

---
+++ Profile.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <locale.h>
+
+int main(){
+    int Idade, calculando, ano, exibe;
+    char nome[30];
+    setlocale(LC_ALL,"portuguese");
+    system("cls");
+    printf("Digite idade: ");
+    scanf("%d",&Idade);
+    printf("Digite seu nome: ");
+    scanf("%s",&nome);
+    ano = 2024-Idade, calculando;
+    printf("Olá, %s,você tem %d anos ,Portanto, você nasceu no ano: %d",nome,Idade ,ano);
+    system("pause>>NULL");
+    return 0;
+}
